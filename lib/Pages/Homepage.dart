f2489ba89e14fb7aae9c1ddb5a28c420d5f3db98
Ravi Bhatt <ravi2018bhatt@gmail.com>
2025-08-30 00:20:10 +0545

    Added the scroll bar

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -10,9 +10,44 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: appBar(),
-        body: Container(),
+    return Scaffold(appBar: appBar(), body: buildContainer());
+  }
+
+  Column buildContainer() {
+    return Column(
+      children: [
+        Container(
+          height: 40,
+          decoration: BoxDecoration(color: Colors.grey),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text("All", style: TextStyle(color: Colors.white, fontSize: 20)),
+              Text(
+                "Favourite",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text(
+                "Completed",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+              Text(
+                "Pending",
+                style: TextStyle(color: Colors.white, fontSize: 20),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: PageView(
+            children: [
+              Container(color: Colors.red[100]),
+              Container(color: Colors.green[100]),
+              Container(color: Colors.blue[100]),
+            ],
+          ),
+        ),
+      ],
     );
   }
 
@@ -29,6 +64,18 @@ class _homepageState extends State<homepage> {
           ),
         ),
       ),
+      leading: IconButton(
+        onPressed: () {},
+        icon: Icon(Icons.search, size: 27),
+        color: Colors.white,
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu, size: 27),
+          color: Colors.white,
+        ),
+      ],
     );
   }
 }
