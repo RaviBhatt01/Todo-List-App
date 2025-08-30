455e8fa92903949e07321f055e3d8730c2a0775c
Ravi Bhatt <ravi2018bhatt@gmail.com>
2025-08-30 18:46:07 +0545

    Added the working TabBar

--- lib/Pages/Homepage.dart
+++ lib/Pages/Homepage.dart
@@ -10,72 +10,74 @@ class homepage extends StatefulWidget {
 class _homepageState extends State<homepage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: appBar(), body: buildContainer());
-  }
-
-  Column buildContainer() {
-    return Column(
-      children: [
-        Container(
-          height: 40,
-          decoration: BoxDecoration(color: Colors.grey),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text("All", style: TextStyle(color: Colors.white, fontSize: 20)),
-              Text(
-                "Favourite",
-                style: TextStyle(color: Colors.white, fontSize: 20),
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.teal,
+          title: Center(
+            child: Text(
+              "Todo Lists",
+              style: TextStyle(
+                fontSize: 25,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
               ),
-              Text(
-                "Completed",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-              Text(
-                "Pending",
-                style: TextStyle(color: Colors.white, fontSize: 20),
-              ),
-            ],
+            ),
           ),
-        ),
-        Expanded(
-          child: PageView(
-            children: [
-              Container(color: Colors.red[100]),
-              Container(color: Colors.green[100]),
-              Container(color: Colors.blue[100]),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-
-  AppBar appBar() {
-    return AppBar(
-      backgroundColor: Colors.teal,
-      title: Center(
-        child: Text(
-          "Todo Lists",
-          style: TextStyle(
-            fontSize: 25,
-            fontWeight: FontWeight.bold,
+          leading: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search, size: 27),
             color: Colors.white,
           ),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.menu, size: 27),
+              color: Colors.white,
+            ),
+          ],
+          bottom: TabBar(
+            labelStyle: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white,
+            ),
+            unselectedLabelStyle: TextStyle(
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+              color: Colors.white54,
+            ),
+            indicatorColor: Colors.white,
+            tabs: [
+              Tab(text: "All"),
+              Tab(text: "Completed"),
+              Tab(text: "Pending"),
+              Tab(text: "Favourites"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Container(
+              color: Colors.red[100],
+              child: Center(child: Text('Hey')),
+            ),
+            Container(
+              color: Colors.grey[100],
+              child: Center(child: Text('Hey2')),
+            ),
+            Container(
+              color: Colors.blue[100],
+              child: Center(child: Text('Hey3')),
+            ),
+            Container(
+              color: Colors.green[100],
+              child: Center(child: Text('Hey4')),
+            ),
+          ],
         ),
       ),
-      leading: IconButton(
-        onPressed: () {},
-        icon: Icon(Icons.search, size: 27),
-        color: Colors.white,
-      ),
-      actions: [
-        IconButton(
-          onPressed: () {},
-          icon: Icon(Icons.menu, size: 27),
-          color: Colors.white,
-        ),
-      ],
     );
   }
 }
